c7f9d20eef6d5547fb0a686bb1a23b6b7a95208b
Ankit Sharma <ankit1sharma2000@gmail.com>
2022-11-15 17:17:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Account from "./Pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </>
+  );
+}
+
+export default App;
